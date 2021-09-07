bc07808e492d41add208780a3038cfc51f4c6af9
amitpanthi <apapanthi@gmail.com>
2021-09-07 16:22:43 +0530

    added swagger api documentation for user routes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose")
+const swaggerUi = require('swagger-ui-express');
 
 const app = express()
 mongoose.connect("mongodb://localhost/fundoo")
+
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json())
 
